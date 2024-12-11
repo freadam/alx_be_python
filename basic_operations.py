6adab77efd53fb14d117acc51fd69a6db1d77815
freadam <freadamabebe@gmail.com>
2024-12-11 13:15:23 +0300

    add basic_operation

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+
+print("Addition of "+str(number1)+" and "+str(number2)+" is "+str(sum))
+print("Subtraction of "+str(number1)+" and "+str(number2)+" is "+str(sub))
+print("Multiplication of "+str(number1)+" and "+str(number2)+" is "+str(mult))
+
+
