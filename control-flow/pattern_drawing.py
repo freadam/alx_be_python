fafed0cac64e91347f3fea21c27a730b20523980
freadam <freadamabebe@gmail.com>
2024-12-17 11:37:52 +0300

    fix 2

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,7 @@ size = int (input("Enter the size of the pattern: "))
 
 for j in range(0,size):
     for i in range (0,size):
-        if(i < size-1):
+        if i < size-1:
             print("*", end="")
         else:
             print("*", end="\n")
