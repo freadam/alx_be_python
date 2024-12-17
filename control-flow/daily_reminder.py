be1fa4413abd50441c3ef01f2247cd7dbf4f60a9
freadam <freadamabebe@gmail.com>
2024-12-17 11:31:32 +0300

    Add control-flow files

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,24 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+message = ""
+
+match priority:
+    case "high":
+        if time_bound=="yes":
+            message ="Reminder: '"+task+"' is a high priority task that requires immediate attention today!"
+        elif time_bound=="no":
+            message ="Note: '"+task+"' is a high priority task.Consider completing it when you have free time."
+    case "medium":
+        if time_bound=="yes":
+            message ="Reminder: '"+task+"' is a medium priority task that requires immediate attention today!"
+        elif time_bound=="no":
+            message ="Note: '"+task+"' is a medium priority task.Consider completing it when you have free time."
+    case "low":
+        if time_bound=="yes":
+            message ="Reminder: '"+task+"' is a low priority task that requires immediate attention today!"
+        elif time_bound=="no":
+            message ="Note: '"+task+"' is a low priority task.Consider completing it when you have free time."
+
+print(message)
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,8 @@
+size = int (input("Enter the size of the pattern: "))
+
+for j in range(0,size):
+    for i in range (0,size):
+        if(i < size-1):
+            print("*", end="")
+        else:
+            print("*", end="\n")
