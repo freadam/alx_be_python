f0fce22e6a468ac0e51b9326dd9a6e0237650aff
freadam <freadamabebe@gmail.com>
2025-01-14 12:05:10 +0300

    fix formatting

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-def display_current_datetime ():
+def display_current_datetime():
     current_date = datetime.now()
     date_time_str = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print('Current date and time: ', date_time_str)
@@ -10,3 +10,4 @@ def calculate_future_date():
     future_date = datetime.today() + timedelta(days=int(input_date))
     date_str = future_date.strftime("%Y-%m-%d ")
     print ("Future date: ", date_str)
+
