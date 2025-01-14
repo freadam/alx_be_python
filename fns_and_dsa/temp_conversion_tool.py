231fa7881a3ea0d145ca661ce150d68cc03fcae5
freadam <freadamabebe@gmail.com>
2025-01-14 12:06:38 +0300

    modify input message

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,7 +8,7 @@ def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
-    temp = float(input("Enter the temperature: "))  
+    temp = float(input("Enter the temperature to convert: "))  
     scale = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
     if scale=="F":
         print(f"{temp}°F is {convert_to_celsius(temp)}°C")
