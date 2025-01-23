6dcbb0ec8cb050195f0ea196fbd728c5cd91fb28
freadam <freadamabebe@gmail.com>
2025-01-23 13:32:35 +0300

    fix 13 naming

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,20 +3,20 @@ from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calculator.add(1, 2), 3)
+        self.assertEqual(self.calc.add(1, 2), 3)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(2, 1), 1)
 
     def test_multiplication(self):
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
 
     def test_division(self):
-        self.assertEqual(self.calculator.divide(6, 2), 3)
-        self.assertIsNone(self.calculator.divide(6, 0))
-        # self.assertRaises(ValueError, self.calculator.divide, 6, 0)
+        self.assertEqual(self.calc.divide(6, 2), 3)
+        self.assertIsNone(self.calc.divide(6, 0))
+        
 if __name__ == '__main__':
     unittest.main()
