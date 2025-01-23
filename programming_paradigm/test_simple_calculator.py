91e52dd3852d56bc946ac4b17f47ec8ee33eab1a
freadam <freadamabebe@gmail.com>
2025-01-23 13:30:59 +0300

    fix 12 naming

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,16 +5,16 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calculator.add(1, 2), 3)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calculator.subtract(2, 1), 1)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calculator.multiply(2, 3), 6)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calculator.divide(6, 2), 3)
         self.assertIsNone(self.calculator.divide(6, 0))
         # self.assertRaises(ValueError, self.calculator.divide, 6, 0)
