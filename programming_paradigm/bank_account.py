676e6e9adf07148bf409ab73f4ce03478d302058
freadam <freadamabebe@gmail.com>
2025-01-23 13:10:04 +0300

    fix display message

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,4 +16,6 @@ class BankAccount:
             return False
 
     def display_balance (self):
-        print(f" Current Balance: ${self.account_balance}.00")
+        print(f" urrent Balance: ${self.account_balance}.00")
+        
+        
