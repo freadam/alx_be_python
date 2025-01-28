bfbbed356d118900468e6e99d621b5c8526e3d9f
freadam <freadamabebe@gmail.com>
2025-01-28 11:07:00 +0300

    added initialization

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,7 +23,9 @@ class PrintBook(Book):
         #PrintBook: The Catcher in the Rye by J.D. Salinger, Page Count: 234
 
 class Library:
-    books = []
+    def __init__(self):
+        self.books = []
+        
     def add_book(self, book):
         self.books.append(book)
     
