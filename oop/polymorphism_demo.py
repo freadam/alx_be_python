d564255ac66213d16bb183a26a05a98e1fb1c8be
freadam <freadamabebe@gmail.com>
2025-01-28 11:04:43 +0300

    Added oop folder and files

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+class Rectangle(Shape):    
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def area(self):
+        return self.width * self.length
+    
